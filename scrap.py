422c2f2763e6cf6b3a57025ab8448c02e3921db7
Andr0mede <andromede.leo@tuta.io>
2024-01-17 12:26:45 +0100

    Cleaning scraper code and adding documentation

--- scrap.py
+++ scrap.py
@@ -7,15 +7,7 @@ Created on Mon Nov 13 17:02:21 2023
 """
 import os
 import cv2
-import numpy as np
-import requests
-from bs4 import BeautifulSoup
-from urllib.parse import urljoin, urlparse
-from urllib.request import urlretrieve
-from selenium import webdriver
 
-
-#%%
 
 class myExtractor:
     
@@ -25,7 +17,6 @@ class myExtractor:
         self._file_path = self._test_zone + file_name
         if not self.checkPath():
             print('Error: Cannot found tmp folder')
-        #self._url = url
         self._img_name = 'payload.jpeg'
         self._img_path = ''
         self._pb_type_name = 'bframe.html'
@@ -35,25 +26,68 @@ class myExtractor:
     
     
     def checkPath(self):
+        """
+        Check if the file path exists. Return a boolean if yes or not it 
+        exists.
+
+        Returns
+        -------
+        TYPE: Boolean
+            DESCRIPTION: If the path exists
+
+        """
         return os.path.exists(self._file_path)
     
     
     def foundPathImg(self):
+        """
+        Found the path to the image(s) of the problem. Return the path to this
+        image(s).
+
+        Returns
+        -------
+        TYPE: String
+            DESCRIPTION: Path to the image problem
+
+        """
+        # Seaching in the folder tmp
         for root, dirs, files in os.walk(self._test_zone):
             if self._img_name in files:
                 return os.path.join(root, self._img_name)
     
+    
     def foundPathPb(self):
+        """
+        Found the path to the html page of the problem. Return the path to this
+        html page.
+
+        Returns
+        -------
+        TYPE: String
+            DESCRIPTION: Path to the html containing the problem
+
+        """
+        # Seaching in the folder tmp
         for root, dirs, files in os.walk(self._test_zone):
             if self._pb_type_name in files:
                 return os.path.join(root, self._pb_type_name)
     
     
     def foundProblem(self):
+        """
+        Deducing the problem type from image format.
+
+        Returns
+        -------
+        TYPE: String
+            DESCRIPTION: Problem type
+
+        """
         # Importing image and retrieving properties
         image = cv2.imread(self._img_path)
         h, w, d = image.shape
         
+        # reCaptcha has two format for the two differents problems
         if h == w and w == 450:
             return 'maskImages'
         elif h == w and w == 300:
@@ -62,7 +96,20 @@ class myExtractor:
             print('Error: Invalid image format')
             return ''
     
+    
     def foundInstruction(self):
+        """
+        Finding the problem instruction by counting the key words occurence. It
+        return the most likely problem instruction.
+
+        Returns
+        -------
+        most_frequent : String
+            DESCRIPTION: Problem instruction to solve
+
+        """
+        # List of instruction in English
+        # TODO: Complete this list if you want with other languages
         list_instruction = ['crosswalk',
                             'bicycle',
                             'boats',
@@ -72,16 +119,28 @@ class myExtractor:
                             'motorcycle',
                             'stairs',
                             'traffic light']
-        with open(self._pb_type_path, 'r') as fichier:
-            contenu = fichier.read()
-            
-            occurences = {string: contenu.count(string) for string in list_instruction}
-            most_frequent = max(occurences, key=occurences.get)
         
+        # Counting occurence of each word in the html file
+        with open(self._pb_type_path, 'r') as file:
+            content = file.read()
+            occurences = {string: content.count(string) for string in list_instruction}
+            
+            # Deducing that the most frequent is the problem instruction
+            most_frequent = max(occurences, key=occurences.get)
+
         return most_frequent
     
     
     def extract3x3Image(self):
+        """
+        Returning list of images for multiImages problem.
+
+        Returns
+        -------
+        list_images : list of np.array
+            DESCRIPTION: All images of the problem
+
+        """
         # Importing image and retrieving properties
         image = cv2.imread(self._img_path)
         h, w, d = image.shape
@@ -102,28 +161,49 @@ class myExtractor:
     
     
     def extract4x4Image(self):
+        """
+        Returning list of image for maskImages problem.
+
+        Returns
+        -------
+        list_images : list of np.array
+            DESCRIPTION: Image of the problem
+
+        """
         # Returning in the same format that the 3x3 extractor
         list_images = [cv2.imread(self._img_path)]
         return list_images
     
     
     def extractProblem(self):
-        
-        # Downloading page
-        #self.compilePage()
-    
+        """
+        Scraping html page and its ressources to deducing problem type,
+        instruction and associated images.
+
+        Returns
+        -------
+        problem_type : String
+            DESCRIPTION: maskImages or multiImages
+        list_images : List of np.array
+            DESCRIPTION: Associated images to the problem
+        problem_instruction : String
+            DESCRIPTION: Instruction of the problem
+
+        """    
         # Checking image exists
         self._img_path = self.foundPathImg()
         if self._img_path == '':
             print('Error: Image does not exists in the specified folder')
         
-        # Checking image exists
+        # Checking html page exists
         self._pb_type_path = self.foundPathPb()
         if self._pb_type_path == '':
             print('Error: File does not exists in the specified folder')
         
         # Finding problem type
         problem_type = self.foundProblem()
+        
+        # Finding problem instruction
         problem_instruction = self.foundInstruction()
         
         # Retrieving images according to problem type
@@ -134,73 +214,6 @@ class myExtractor:
         elif problem_type == 'multiImages':
             list_images = self.extract3x3Image()
         
-        
-        
         return problem_type, list_images, problem_instruction
 
 
-    def compilePage(self):
-        url = self._file_path
-        print(url)
-        # Créer une instance du navigateur Firefox (le chemin vers GeckoDriver n'est pas nécessaire ici)
-        driver = webdriver.Firefox()
-        # Accéder à l'URL local
-        driver.get(url)
-        # Attendre que la page soit complètement chargée (vous pouvez ajuster le délai selon vos besoins)
-        driver.implicitly_wait(10)
-        # Récupérer le code HTML après le chargement complet
-        html_content = driver.page_source
-        # Fermer le navigateur
-        driver.quit()
-        # Enregistrer le code HTML dans un fichier
-        with open("page_compiled.html", "w", encoding="utf-8") as html_file:
-            html_file.write(html_content)
-"""
-    def downloadPage(self):
-        # Télécharger le contenu de la page
-        response = requests.get(self._url)
-        html_content = response.text
-    
-        # Créer un objet BeautifulSoup pour analyser le HTML
-        soup = BeautifulSoup(html_content, 'html.parser')
-    
-        # Télécharger les ressources externes
-        for tag in soup.find_all(['img', 'link', 'script']):
-            if 'src' in tag.attrs:
-                resource_url = tag['src']
-            elif 'href' in tag.attrs:
-                resource_url = tag['href']
-            else:
-                continue
-    
-            absolute_url = urljoin(self._url, resource_url)
-            self.downloadResource()
-    
-        # Enregistrer le HTML
-        with open(os.path.join(self._path, 'index.html'), 'w', encoding='utf-8') as html_file:
-            html_file.write(html_content)
-    
-    def downloadResource(self):
-        # Télécharger la ressource et l'enregistrer localement
-        filename = os.path.basename(urlparse(self._url).path)
-        local_path = os.path.join(self._path, filename)
-        print(self._path)
-        urlretrieve(self._url, self._path)
-        print(f"Ressource téléchargée : {self._url}")
-    
-    
-
-
-if __name__ == "__main__":
-    # Spécifier l'URL de la page à enregistrer
-    page_url = "https://www.example.com"
-
-    # Spécifier le dossier de sortie
-    output_folder = "output_folder"
-
-    # Créer le dossier de sortie s'il n'existe pas
-    os.makedirs(output_folder, exist_ok=True)
-
-    # Télécharger la page web et ses ressources
-    download_page(page_url, output_folder)
-"""
